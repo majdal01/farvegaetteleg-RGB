61ab6261307e4fec3c07d5dbffac8ce29b3e7519
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2025-09-22 12:41:11 +0200

    Tilføjet nytSpil(), hvor den sætter rigtige farve i html, danner 9 andre farver og pusher i fælles array (farveArray), samt blander farven ved hjælp af Fisher-Yates shuffle.

--- script.js
+++ script.js
@@ -2,13 +2,42 @@
 
 let rgbKode = document.getElementById("rgb-koden");
 
-
+// Generer RGB koder
 function genererRGB() {
     let rød = Math.floor(Math.random()*255+1);
     let blå = Math.floor(Math.random()*255+1);
-    let grøn = Math.floor(Math.random()*255+1);
-
-    rgbKode.textContent = `RGB (${rød}, ${blå}, ${grøn})`;
+    let grøn = Math.floor(Math.random()*255+1);   
+    return `RGB (${rød}, ${blå}, ${grøn})`;
 }
 
-console.log(genererRGB())
+function nytSpil() {
+    let rigtigeFarve = genererRGB();
+    rgbKode.textContent = rigtigeFarve;
+
+    //Danner nu det array af farver, der skal vises
+    let farveArray = [];
+    farveArray.push(rigtigeFarve);
+
+    for( let i = 0; i < 9; i++){
+        let nyeFarver = genererRGB();
+        while (nyeFarver === rigtigeFarve){ //Kører indtil betingelsen er falsk
+            nyeFarver = genererRGB();
+        }
+        farveArray.push(nyeFarver); //Hvis falsk, så push
+    }
+
+    //Array af farver blandes (Fisher-Yates shuffle algoritmen) - ref: https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
+    function bland(farveArray) {
+        for (let i = farveArray.length - 1; i > 0; i--) { 
+            const j = Math.floor(Math.random() * (i + 1)); 
+            [farveArray[i], farveArray[j]] = [farveArray[j], farveArray[i]]; 
+            } 
+        return farveArray; 
+        }; 
+    
+    let blandetArray = bland(farveArray);
+    console.log(blandetArray)
+
+};
+
+nytSpil()
